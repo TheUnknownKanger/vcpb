987551464ec6d71fefc7f9ac7769c28300dafdd0
MysteryxD <75388792+MysteryxD@users.noreply.github.com>
2021-01-27 21:07:58 +0530

    About 👀

---
+++ handlers/about.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+from pyrogram.handlers import MessageHandler
+from handlers import all_help
+from helpers import wrap
+from strings import get_string as _
+
+@Client.on_message(
+    filters.command("about", "/") & filters.private
+)
+@wrap
+def rules(client, message):
+    message.reply_text(_("creds")) 
+
+__handlers__ = [
+    [
+        MessageHandler(
+            rules,
+            filters.command("about", "/")
+            & filters.private
+        )
+    ]
+]
+__help__ = {
+    "help": [_("help_about"), False]
+}
